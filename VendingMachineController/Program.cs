f48934e170d73fc898b180b68fca94157e8b641b
Simonas Marcišauskas <11994076+simas232@users.noreply.github.com>
2021-08-02 00:22:25 +0300

    fix: introduce selection menu of VendingMachine items in Program.cs

--- VendingMachineController/Program.cs
+++ VendingMachineController/Program.cs
@@ -8,7 +8,6 @@ namespace VendingMachineController
     {
         static void Main(string[] args)
         {
-
             VendingMachine vMachine = new VendingMachine();
             vMachine.ProductArray.Add(new Drink(1, "Orange-flavored drink", 11, 330));
             vMachine.ProductArray.Add(new Drink(2, "Mineral water", 15, 500));
@@ -20,18 +19,69 @@ namespace VendingMachineController
             vMachine.ProductArray.Add(new Toy(8, "Fidget spinner", 10, "red"));
             vMachine.ProductArray.Add(new Toy(9, "Whistle", 5, "yellow"));
 
-            vMachine.ShowAll();
-
-            int userSelection;
+            char userSelection;
             do
             {
                 Console.Clear();
-                Console.WriteLine();
-                userSelection = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(vMachine.ShowAll());
+                Console.WriteLine($"Money Balance: {vMachine.MoneyPool} kr");
+                Console.WriteLine("Press 1-9 to Buy An Item, R to Insert Money or Q to Finish Purchases");
+                userSelection = Convert.ToChar(Console.ReadLine());
 
-            } while (userSelection != 0);
+                switch (userSelection)
+                {
+                    case '1':
+                        BuyItemFromVendingMachine(vMachine, 1);
+                        break;
+                    case '2':
+                        BuyItemFromVendingMachine(vMachine, 2);
+                        break;
+                    case '3':
+                        BuyItemFromVendingMachine(vMachine, 3);
+                        break;
+                    case '4':
+                        BuyItemFromVendingMachine(vMachine, 4);
+                        break;
+                    case '5':
+                        BuyItemFromVendingMachine(vMachine, 5);
+                        break;
+                    case '6':
+                        BuyItemFromVendingMachine(vMachine, 6);
+                        break;
+                    case '7':
+                        BuyItemFromVendingMachine(vMachine, 7);
+                        break;
+                    case '8':
+                        BuyItemFromVendingMachine(vMachine, 8);
+                        break;
+                    case '9':
+                        BuyItemFromVendingMachine(vMachine, 9);
+                        break;
+                    case 'R':
+                        Console.WriteLine("Enter The Money Sum to Be Added (1, 5, 10, 20, 50, 100, 500 or 1000)");
+                        vMachine.InsertMoney(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 'Q':
+                        return;
+                }
+                Console.WriteLine("Press <Spacebar> to Continue... ");
+                while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            } while (true);
+        }
 
-            //Console.WriteLine("Hello World!");
+        public static void BuyItemFromVendingMachine(VendingMachine vMachine, int itemId)
+        {
+            Product boughtProduct;
+            boughtProduct = vMachine.Purchase(itemId);
+            if (boughtProduct == null)
+            {
+                Console.WriteLine("Not Enough Money! Insert More Money And Try Again.");
+            }
+            else
+            {
+                Console.WriteLine($"You Bought {vMachine.ProductArray[itemId-1].Name}");
+                Console.WriteLine(boughtProduct);
+            }
         }
     }
 }
